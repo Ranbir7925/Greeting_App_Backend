3937138e7e5f5349e931c3da0e44ab81d7c7f376
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-07 20:16:35 +0530

    refactor:DataBase configiuration moved to another file

--- Server.js
+++ Server.js
@@ -1,20 +1,16 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const dbConfig = require('./config/dbConfig.js')
-const mongoose = require('mongoose')
 const app = express()
-require('./app/routes/greeting.route.js')(app)
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-mongoose.connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Successfully connected to the database"))
-    .catch(err => {
-        console.log('Could not connect to the database. Exiting now...', err);
-        process.exit()
-    })
-app.listen(3000, () => console.log("Server is listening on port 3000"))
+
+require('./app/routes/greeting.route.js')(app)
+require('./config/dbConfig')
+
+const PORT = 4000
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
