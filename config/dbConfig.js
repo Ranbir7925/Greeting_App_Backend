d352f93df686bc585aa1d86d10b4d64f7ac7d320
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-07 20:17:44 +0530

    add:DataBase connection done sepratly

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,3 +1,10 @@
-module.exports = {
-    url: 'mongodb://localhost:27017/GreetingApp'
-}
+require('dotenv/config')
+const mongoose = require('mongoose')
+const DB_CONFIG = mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Successfully connected to the database"))
+    .catch(err => {
+        console.log('Could not connect to the database. Exiting now...', err);
+        process.exit()
+    })
+
+ module.exports = DB_CONFIG
