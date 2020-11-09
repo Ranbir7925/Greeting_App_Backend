9ebdfcc9aba5e7e6e36e56590b8c564490f3b8a8
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-09 21:25:21 +0530

    refactor:Removed unused parameters

--- Server.js
+++ Server.js
@@ -1,16 +1,32 @@
-
+/*************************************************************
+ *
+ * Execution       : default node cmd> node server.js
+ * Purpose         : Run a nodejs server and connect to a database server
+ *
+ * @description    : Creates a app using express ,and add body-parser middlewares 
+ *                   using express’s app.use() method. We connect to the database using mongoose.
+ *                   We run a nodejs server which listens on port number 4000. 
+ * @file           : Server.js
+ * @author         : Ranbir Singh
+ * @overview       : Run a nodejs server and connect to a database server
+ * @version        : 1.0
+ * @since          : 08/11/2020
+ *
+ * **********************************************************/
+require('dotenv/config')
+const logger = require('./logger')
 const express = require('express')
 const bodyParser = require('body-parser')
+// const PORT = 4000
+require('./config/dbConfig')
+/**
+ * create express app
+ */
 const app = express()
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
+require('./app/routes/greeting.rts.js')(app)
 
-
-require('./app/routes/greeting.route.js')(app)
-require('./config/dbConfig')
-
-const PORT = 4000
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`))
+logger.info("Server started")
