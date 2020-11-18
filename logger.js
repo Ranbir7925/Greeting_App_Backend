19e91472793af9594b4171873a7ada1361e639ce
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-19 01:02:00 +0530

    add:Code to log seprately

--- logger.js
+++ logger.js
@@ -1,10 +1,15 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-    levels: winston.config.syslog.levels,
+    // levels: winston.config.syslog.levels,
     transports: [
         new winston.transports.File({
-            filename: 'combined.log'
+            filename: 'logs/error.log',
+            level: 'error'
+        }),
+        new winston.transports.File({
+            filename: 'logs/info.log',
+            level:'info'
         })
     ]
 });
